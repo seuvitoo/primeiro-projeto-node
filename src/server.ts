292f0796de0d4c4d6f5357c4ddd2b9957128b351
Victor dos Santos <vy.santos@live.com>
2020-04-14 17:45:50 -0300

    eslint config

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
-import express, { response } from 'express';
+import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Helo GoStack' })
-})
+app.get('/', (request, response) => response.json({ message: 'Helo GoStack' }));
 
 app.listen(3333, () => {
-  console.log('🎉 Server started on port 3333!')
+  console.log('🎉 Server started on port 3333!');
 });
